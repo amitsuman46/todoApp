0b28cc8a8701a5763c59df5e2513788823f7632c
Amit Suman <amit.suman456@gmail.com>
2023-08-27 16:40:28 +0530

    changes in app 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Amti Suman <code>src/App.js</code> and save to reload.
+          Amti Suman is a good boy <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
