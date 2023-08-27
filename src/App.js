81c0b84e699ea8445e69781e9a66118b72cf07d7
Amit Suman <amit.suman456@gmail.com>
2023-08-27 16:44:56 +0530

    removed junk code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Amti Suman is a good boy <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>pro Developers</h2>
     </div>
   );
 }
